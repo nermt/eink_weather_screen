1668c1df5ca303a5b29b76a0f14ac6584468ec8a
Markus Ekdahl <markus@familjenekdahl.com>
2021-06-03 14:45:44 +0200

    Test to read sensor values from Netatmo

--- eink_weather_screen.py
+++ eink_weather_screen.py
@@ -1,6 +1,26 @@
 import sys
-import netatmo
+import configparser
+import lnetatmo
 
-ws = netatmo.WeatherStation()
+config = configparser.ConfigParser()
+config.read('config.ini')
 
-print ('test')
+authData = lnetatmo.ClientAuth( clientId = config['NETATMO']['ClientID'],
+                                clientSecret = config['NETATMO']['ClientSecret'],
+                                username = config['NETATMO']['Username'],
+                                password = config['NETATMO']['Password'])
+
+weatherData = lnetatmo.WeatherStationData(authData)
+print ('Current temperature indoor: %s °C, trend is: %s' % (weatherData.lastData()['Vardagsrum']['Temperature'],
+                                                            weatherData.lastData()['Vardagsrum']['temp_trend']))
+
+print ('Current temperature outdoor: %s °C, trend is: %s' % (weatherData.lastData()['Utomhus']['Temperature'],
+                                                            weatherData.lastData()['Utomhus']['temp_trend']))
+
+print ('Battery of outdoor temperature sensor is: %d' % weatherData.lastData()['Utomhus']['battery_percent'])
+
+print ("It's currently raining with %s mm/h, totally %s mm over the last 24h" % (weatherData.lastData()['Regnmätare']['Rain'],
+                                                                              weatherData.lastData()['Regnmätare']['sum_rain_24']))
+
+print ('Battery of rain sensor is: %d' % weatherData.lastData()['Regnmätare']['battery_percent'])
+print ('Done')
